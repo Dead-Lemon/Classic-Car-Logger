a965e2c1243b51d2e7d1cdc5c6140cfa715be77a
Dead-Lemon <wslatem@gmail.com>
2021-03-30 21:51:45 +0200

    start up message XD

--- Car-Display-Console/Car-Display-Console.ino
+++ Car-Display-Console/Car-Display-Console.ino
@@ -58,6 +58,13 @@ void setup() {
   lcd3.clear();
   lcd4.clear();
 
+  lcd1.print("SPEEDOPHILE");
+  lcd2.print("TOUCHING");
+  lcd3.print("YOUR");
+  lcd4.print("CAR");
+
+  delay(2000);  
+
   
 }
 
