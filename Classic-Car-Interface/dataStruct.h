3a27b67c7791b23f0ac5cab140c0af69305d4c04
Dead-Lemon <wslatem@gmail.com>
2021-03-30 17:47:44 +0200

    added logging structure

--- Classic-Car-Interface/dataStruct.h
+++ Classic-Car-Interface/dataStruct.h
@@ -14,7 +14,7 @@ struct gpsDataStruct {
     uint32_t date, time, age;
     uint8_t satellites;
     float speed, alt, course, distanceToStart;
-    uint16_t laps = 0;
+    uint16_t laps;
     
 } gpsData; //all data that will be logged
 
@@ -35,3 +35,19 @@ struct sensorData {
   float batVolt, engineTemp, oilPress, rpm;
   
 } engineSensor;
+
+struct loggingStruct
+{
+  uint8_t slaveID;
+  uint8_t senseorder;
+  bool warnstatus;
+  uint8_t sensefault;
+  uint8_t senseglyph;
+  float sensevals;
+  int8_t units;
+  const float sensemaxvals;
+  const float senseminvals;
+  const float sensewarnhivals;
+  const float sensewarnlowvals;
+  const char sensename[13];
+} logger;
