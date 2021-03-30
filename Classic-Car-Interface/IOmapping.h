80d0a60275d6809b7c0894f4c1cdf01c62f0bc65
Dead-Lemon <wslatem@gmail.com>
2021-03-30 15:44:54 +0200

    seperate IO mapping

--- Classic-Car-Interface/IOmapping.h
+++ Classic-Car-Interface/IOmapping.h
@@ -1,3 +1,3 @@
-const uint8_t oilPressInput = A1;
-const uint8_t tachoPin = PA1;
+const uint8_t oilPressInput = A1; //as it says in the name.
+const uint8_t tachoPin = PA1; //input from distributor negative. recommded using circuit to clean up this input
 const uint8_t engineTempInput = A0; //engine temp sensor input from voltage divider
