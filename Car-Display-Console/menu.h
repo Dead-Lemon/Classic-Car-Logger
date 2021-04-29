a396be3d46af5ffd0568a2bd2032a2b43c5c511a
Dead-Lemon <wslatem@gmail.com>
2021-04-29 14:29:10 +0200

    menu stuff

--- Car-Display-Console/menu.h
+++ Car-Display-Console/menu.h
@@ -5,7 +5,20 @@
 
 class menuSystem {
   public:
-    
+    menuSystem()
+  private:
+    uint8_t _not
+}
+
+typedef struct {
+
+  uint8_t menuIndex;
+  String menuName;
+  
+} menuList;
+
+menuList mainMenu[] = {
+  {0, "MENU"};
 }
 
 
